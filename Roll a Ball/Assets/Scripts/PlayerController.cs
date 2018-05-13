4fe38b7fa8840a8900b6f7a1eb02bddf0c282d75
Fei Liu <Kechushou@users.noreply.github.com>
2018-05-13 15:08:38 -0500

    Roll a ball game
    Finished the roll a ball game tutorial 6

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -24,4 +24,12 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce(movement * speed);
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
