719c60b066a5f59aed7c41d63092f180756eebd7
Fei Liu <Kechushou@users.noreply.github.com>
2018-05-13 17:26:50 -0500

    Roll a ball game
    Finished roll a ball game tutorial

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -25,11 +32,23 @@ public class PlayerController : MonoBehaviour {
 
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+            
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
