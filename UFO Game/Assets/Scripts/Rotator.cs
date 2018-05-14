34bfea100bced3a0a6d0971f53dff08125b28d03
Fei Liu <Kechushou@users.noreply.github.com>
2018-05-14 15:27:16 -0400

    2D UFO Game
    Finished 2D UFO Game tutorial 5/9

---
+++ UFO Game/Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+	}
+}
