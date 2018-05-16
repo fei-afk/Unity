f32c319fc0853cf0d233f9fa4e293dd63d9445fa
Fei Liu <Kechushou@users.noreply.github.com>
2018-05-15 20:32:23 -0400

    Survial Shooter Game
    Create Survial Shooter Game Tutorial 1/10

---
+++ Survival Shooter Gane/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
