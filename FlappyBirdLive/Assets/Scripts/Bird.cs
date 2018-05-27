54f9aa540a057a3231114dc6922ae09abd13f313
Fei Liu <Kechushou@users.noreply.github.com>
2018-05-27 17:15:08 -0400

    Flappy Bird Update
    Finished Flappy Bird Tutorial 3/10

---
+++ FlappyBirdLive/Assets/Scripts/Bird.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+	}
+
+    void OnCollisionEnter2D()
+    {
+        isDead = true;
+    }
+}
